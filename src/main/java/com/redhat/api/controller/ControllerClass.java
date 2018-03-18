282eaccc27001b266fc2e85d9c64bbf4eea99d0d
Richard <richyonstrings@gmail.com>
2018-03-18 23:04:26 +0530

    First Open Shift

---
+++ src/main/java/com/redhat/api/controller/ControllerClass.java
@@ -0,0 +1,28 @@
+package com.redhat.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControllerClass {
+
+	@RequestMapping(value= {"/","/home"}, method=RequestMethod.GET)
+	public ModelAndView goHome() {
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("homePage");
+		
+		return model;
+	}
+	
+	@RequestMapping(value= {"/check"}, method=RequestMethod.GET)
+	public ModelAndView goCheck() {
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("check");
+		
+		return model;
+	}
+}
